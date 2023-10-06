eabdd9aeb0c21573fca2e02fc19fd39084ee0ad1
AkhiSharmin <akhisharmin100@gmail.com>
2023-10-07 00:54:35 +0600

    added packingList

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+  { id: 2, description: "Charger", quantity: 1, packed: false }
+]
+
 export default function App() {
-  return <>
+  return <div className="app">
     <Logo />
     <Form />
     <PackingList />
     <Starts />
-  </>
+  </div>
 }
 
 function Logo() {
@@ -16,10 +22,25 @@ function Form() {
   </div>
 }
 function PackingList() {
-  return <div className="list">LIST</div>
+  return <div className="list">
+    <ul>
+      {initialItems.map(item => <Item item={item} />)}
+    </ul>
+  </div>
 }
+
+function Item({ item }) {
+  return <li>
+    <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+      {item.quantity} {item.description}
+    </span>
+    <button>❌</button>
+  </li>
+}
+
+
 function Starts() {
-  return <footer>
+  return <footer className="stats">
     <en>You have X items on your list, and you already packed X (X%)</en>
   </footer>
 }
