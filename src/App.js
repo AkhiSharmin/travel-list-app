ee8b7f91f113936b351b4d881946e3c08d29fb02
AkhiSharmin <akhisharmin100@gmail.com>
2023-10-07 01:37:26 +0600

    added form

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: true },
-  { id: 2, description: "Charger", quantity: 1, packed: false }
+  { id: 3, description: "Charger", quantity: 1, packed: false }
 ]
 
 export default function App() {
@@ -16,18 +16,40 @@ export default function App() {
 function Logo() {
   return <h1>🏝️ Far Away 🧳</h1>
 }
+
+
 function Form() {
-  return <div className="add-form">
+
+  function handelSubmit(e) {
+    e.preventDefault();
+    console.log(e);
+  }
+
+
+  return <form className="add-form" onSubmit={handelSubmit}>
     <h3>What do you need for your 😍 trip?</h3>
-  </div>
+    <select>
+      {Array.from({ length: 20 }, (_, i) => i + 1).map(num => (
+        <option value={num} key={num} >
+          {num}
+        </option>))}
+    </select>
+    <input type="text" placeholder="Item...." />
+    <button>Add</button>
+  </form>
 }
+
+
+
 function PackingList() {
   return <div className="list">
     <ul>
-      {initialItems.map(item => <Item item={item} />)}
+      {initialItems.map(item => <Item item={item} key={item.id} />)}
     </ul>
   </div>
 }
+
+
 
 function Item({ item }) {
   return <li>
