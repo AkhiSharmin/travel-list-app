df8f9aed46eaf8845ad0812d499f2c90ddaf3763
AkhiSharmin <akhisharmin100@gmail.com>
2023-10-07 13:30:24 +0600

    added logo

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: true },
@@ -19,6 +21,8 @@ function Logo() {
 
 
 function Form() {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
 
   function handelSubmit(e) {
     e.preventDefault();
@@ -28,13 +32,13 @@ function Form() {
 
   return <form className="add-form" onSubmit={handelSubmit}>
     <h3>What do you need for your 😍 trip?</h3>
-    <select>
+    <select value={quantity} onChange={e => setQuantity(e.target.value)}>
       {Array.from({ length: 20 }, (_, i) => i + 1).map(num => (
         <option value={num} key={num} >
           {num}
         </option>))}
     </select>
-    <input type="text" placeholder="Item...." />
+    <input type="text" placeholder="Item...." value={description} onChange={(e) => setDescription(e.target.value)} />
     <button>Add</button>
   </form>
 }
